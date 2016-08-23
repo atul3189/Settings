b8382fd33b5d84d811b43d201c54d13f21c29f37
Atul M <atul3189@gmail.com>
2016-08-23 21:00:26 +0530

    Show cancel button while download in progress in Font screen

--- settings/ViewControllers/Appearance/PKFontCreateViewController.m
+++ settings/ViewControllers/Appearance/PKFontCreateViewController.m
@@ -36,6 +36,8 @@
 {
     if ([self.navigationController.viewControllers indexOfObject:self] == NSNotFound) {
         [self performSegueWithIdentifier:@"unwindFromAddFont" sender:self];
+        [PKSettingsFileDownloader cancelRunningDownloads];
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
     }
     [super viewWillDisappear:animated];
 }
@@ -69,7 +71,7 @@
 - (IBAction)importButtonClicked:(id)sender{
     if(_urlTextField.text.length > 4 && [[_urlTextField.text substringFromIndex:[_urlTextField.text length]-4]isEqualToString:@".css"]){
         [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
-        self.importButton.enabled = NO;
+        [self configureImportButtonForCancel];
         self.urlTextField.enabled = NO;
         [PKSettingsFileDownloader downloadFileAtUrl:_urlTextField.text withCompletionHandler:^(NSData *fileData, NSError *error) {
             if(error == nil){
@@ -82,7 +84,7 @@
                     [self presentViewController:alertController animated:YES completion:nil];
                     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
                     self.urlTextField.enabled = YES;
-                    self.importButton.enabled = YES;
+                    [self reconfigureImportButton];
                 });
             }
         }];
@@ -96,6 +98,7 @@
 
 - (void)downloadCompletedWithFilePath:(NSData*)fileData{
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    [self.importButton setTitle:@"Download Complete" forState:UIControlStateNormal];
     self.importButton.enabled = NO;
     _downloadCompleted = YES;
     _tempFileData = fileData;
@@ -109,7 +112,7 @@
         NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
         NSString *documentsDirectory = [paths objectAtIndex:0];
         NSString *folderPath = [NSString stringWithFormat:@"%@/FontsDir/",documentsDirectory];
-        NSString *filePath = [NSString stringWithFormat:@"%@/%@", folderPath,self.nameTextField.text];
+        NSString *filePath = [NSString stringWithFormat:@"%@/%@.css", folderPath,self.nameTextField.text];
         [[NSFileManager defaultManager] createDirectoryAtPath:folderPath withIntermediateDirectories:YES attributes:nil error:nil];
         NSError *error;
         [_tempFileData writeToURL:[NSURL fileURLWithPath:filePath] options:NSDataWritingAtomic error:&error];
@@ -119,5 +122,23 @@
     }
 }
 
+- (IBAction)cancelButtonTapped:(id)sender{
+    [PKSettingsFileDownloader cancelRunningDownloads];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    self.urlTextField.enabled = YES;
+    [self reconfigureImportButton];
+}
 
+
+- (void)configureImportButtonForCancel{
+    [self.importButton setTitle:@"Cancel download" forState:UIControlStateNormal];
+    [self.importButton setTintColor:[UIColor redColor]];
+    [self.importButton addTarget:self action:@selector(cancelButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)reconfigureImportButton{
+    [self.importButton setTitle:@"Import" forState:UIControlStateNormal];
+    [self.importButton setTintColor:[UIColor blueColor]];
+    [self.importButton addTarget:self action:@selector(importButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+}
 @end
